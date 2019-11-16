2b713d8b784b9cd8f04acdc62e166da5e5106ea7
byronkamal <byronkamal@hotmail.com>
2019-11-16 15:06:51 -0300

    Testes no EditMonitoring

--- src/Components/__tests__/EditMonitoring.test.js
+++ src/Components/__tests__/EditMonitoring.test.js
@@ -22,9 +22,27 @@ let props={
 
 
 describe('Testing EditMonitoring component', () => {
+
     it('Test if EditMonitoring renders correctly', () =>{
         jest.fn();
         const tree = shallow(<EditMonitoring {...props}/>);
         expect(toJson(tree)).toMatchSnapshot();
+    });
+
+    it('should call onChange in Text Field #name', () => {
+        
+        // Evento necessario para mudar o valor do event.target.value na função
+        const event = {
+          target: { value: 'Name Test' }
+        };
+
+        const component = shallow(<EditMonitoring {...props}/>);
+
+        // Achar o TextField com id="name" e simular o evento de mudança no campo
+        component.find('#name').simulate('change', event);
+        
+        // Conferindo se o valor do campo #name é igual ao que foi 'setado'
+        // no passo anterior. Isso me garante que o campo de nome recebeu o valor "Name Test" 
+        expect(component.find('#name').props().value).toEqual('Name Test');
     });
 });
